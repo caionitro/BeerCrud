03d3c43e3588b8912c9dcb16c6c83eab42006cce
Cristiano Santos <cristianossd@gmail.com>
2016-10-30 13:07:06 -0300

    Configuring template engine

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var nunjucks = require('nunjucks');
 var db = require('./config/db');
+var env = require('./config/env');
 var Beer = require('./models/beer');
 var router = express.Router();
 
@@ -14,13 +16,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('views', env.views);
+app.engine('html', nunjucks.render);
+var nunjucksEnv = nunjucks.configure(env.views, {
+  autoscape: true,
+  express: app
+});
+
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 
-require('./routes/routes')(router,Beer); 
+require('./routes/routes')(router,Beer);
 
 app.get('/', function(req, res) {
-	res.sendfile('./public/views/index.html'); // carrega o public/views/index.html //hacktoberfest 2016
+	res.render('index.html');
 });
 
 app.use('/api/',router);
