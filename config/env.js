d60ff521072d69c61b29b161487c6cfa1f6cdba7
rdgoutiyama <rdgo.utiyama@gmail.com>
2019-10-01 09:08:10 -0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 var path = require('path');
-
 var rootDir = path.join(__dirname, '..');
 var publicDir = path.join(rootDir, 'public');
 
 module.exports = {
   views: path.join(publicDir, 'views/')
 };
+
